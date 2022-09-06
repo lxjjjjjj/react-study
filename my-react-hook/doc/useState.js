9a766b67f7879ac07b79db31f7b43dbd835a3c32
“lxjjjjjj” <1732749825@qq.com>
2022-09-06 23:30:44 +0800

    修改

--- my-react-hook/doc/useState.js
+++ my-react-hook/doc/useState.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-let lastState = []
-let stateIndex = 0
-function useState(initState) {
-    lastState[stateIndex] = lastState[stateIndex] || initState;
-    const currentIndex = stateIndex
-    function setState(newState) {
-        lastState[stateIndex] = newState
-        render()
-    }
-    return [lastState[stateIndex++],setState]
+let states = []
+let setters = []
+let cursor = 0
+
+//  使用工厂模式生成一个 createSetter，通过 cursor 指定指向的是哪个 state
+function createSetter(cursor) {
+  return function(newVal) { // 闭包
+    states[cursor] = newVal
+    render()
+  }
+}
+// 每次重新渲染都能准确找到对应的setter和state的秘诀
+// 就是在每次render函数执行的时候都重置cursor
+// 这样就能找到之前存放在states里的state值
+// 不会导致cursor的值一直增加 导致states数组的长度和setter数组的长度一直增加
+function useState(initVal) {
+  states.push(initVal)
+  setters.push(createSetter(cursor))
+  let state = states[cursor]
+  let setter = setters[cursor]
+  // 光标移动到下一个位置
+  cursor++
+  // 返回
+  return [state, setter]
 }
 function App(){
     const [count, setCount] = useState(0);
@@ -23,7 +37,7 @@ function App(){
             <button
                 onClick={() => {
                     setCount(count + 1);
-                    setSum(sum+2)
+                    setSum(sum + 2)
                 }}
             >
                 增加
@@ -32,6 +46,7 @@ function App(){
     );
 }
 function render(){
+    cursor = 0
     ReactDOM.render(
         <App />,
         document.getElementById('root')
