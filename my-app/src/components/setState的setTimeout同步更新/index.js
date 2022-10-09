82ff0db6055b4ea7bbc1ef5062df2af2453ab582
lxjjjjjj <1732749825@qq.com>
2022-10-09 14:17:56 +0800

    修改

--- my-app/src/components/setState的setTimeout同步更新/index.js
+++ my-app/src/components/setState的setTimeout同步更新/index.js
@@ -10,14 +10,21 @@
     }
   
     // 在事件处理函数中setState的调用会批量异步执行
-    // 但是加了setTimeout之后，更新就是同步的
+    // 但是加了setTimeout之后，更新就是同步的 会一次加两个数
+    // 但是如果不加异步的话 就是虽然执行了两次相加操作 但是只会加一次数
     handleClick = (event) => {
-        setTimeout(() => {
-            this.setState({ number: this.state.number + 1 });
-            console.log(this.state); // 1
-            this.setState({ number: this.state.number + 1 });
-            console.log(this.state); // 2
-        });
+        // setTimeout(() => {
+        //     this.setState({ number: this.state.number + 1 });
+        //     console.log(this.state); // 1
+        //     this.setState({ number: this.state.number + 1 });
+        //     console.log(this.state); // 2
+        // });
+        // Promise.resolve().then(() => {
+			this.setState({ number: this.state.number + 1 });
+			console.log(this.state); // 1
+			this.setState({ number: this.state.number + 1 });
+			console.log(this.state); // 2
+		// });
     };
   
     render() {
